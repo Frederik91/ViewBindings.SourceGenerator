2466bde9569062a88ea5ecfc91aefbecccb0d05a
Frederik91 <11948105+Frederik91@users.noreply.github.com>
2025-09-06 08:53:37 +0200

    fix: make generated attributes internal

---
+++ ViewBindings.SourceGenerator.Tests/Snapshots/ViewBindingsSourceGeneratorTests.TwoViewModels#ViewBindingAttributes.g.verified.cs
@@ -0,0 +1,31 @@
+﻿//HintName: ViewBindingAttributes.g.cs
+
+using System;
+
+namespace ViewBindings.SourceGenerator.Contracts.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+internal sealed class ViewBindingAttribute : Attribute
+{
+    public Type? ViewType { get; set; }
+
+    public ViewBindingAttribute()
+    {
+    }
+
+    public ViewBindingAttribute(Type viewType)
+    {
+        ViewType = viewType;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Assembly)]
+internal sealed class ViewBindingsNamespaceAttribute : Attribute
+{
+    public string Namespace { get; }
+
+    public ViewBindingsNamespaceAttribute(string @namespace)
+    {
+        Namespace = @namespace;
+    }
+}
